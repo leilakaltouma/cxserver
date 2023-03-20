8d31acc3ce725afa433cfc0729016bd6bdd62b19
Leila <yasminekaltouma@gmail.com>
2023-03-20 21:20:57 +0000

    second commit

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { createObjectCsvWriter } from "csv-writer";
 import { TransformLanguages } from "./config/transform.js";
 
 const csvWriter = createObjectCsvWriter({
-  path: "langs.csv",
+  path: "output.csv",
   header: [
     { id: "sourceLanguage", title: "source language" },
     { id: "targetLanguage", title: "target language" },
-    { id: "engineName", title: "translation engine is" },
-    { id: "preferedEngine", title: "preferred engine" },
+    { id: "engineName", title: "translation engine" },
+    { id: "preferedEngine", title: "is preferred engine?" },
   ],
 });
 
@@ -32,9 +32,9 @@ const mtDefaults = YAML.load(
   fs.readFileSync(`./config/mt-defaults.wikimedia.yaml`, "utf8")
 );
 
-//loop trough each file
-for (let i = 0; i < files.length; i++) {
-  const file = fs.readFileSync(`./config/${files[i]}.yaml`, "utf8");
+//loop trough each fileName
+files.forEach(baseName  => {
+  const file = fs.readFileSync(`./config/${baseName}.yaml`, "utf8");
   let doc = YAML.load(file);
 
   //check if 'transform.js' needs to be used
@@ -44,20 +44,20 @@ for (let i = 0; i < files.length; i++) {
   }
 
   //loop through the values
-  for (const [key, value] of Object.entries(doc)) {
-    value.forEach((x) => {
+  for (const [sourceLanguage, targetLanguages] of Object.entries(doc)) {
+    targetLanguages.forEach((targetLanguage) => {
       result.push({
-        sourceLanguage: key,
-        targetLanguage: x,
-        engineName: files[i],
+        sourceLanguage: sourceLanguage,
+        targetLanguage: targetLanguage,
+        engineName: baseName,
 
-        //check if the engine is the default engine for that lnaguage pair
+        //check if the engine is the default engine for that language pair
         preferedEngine:
-          mtDefaults[`${key}-${x}`] === files[i] ? "true" : "false",
+          mtDefaults[`${sourceLanguage}-${targetLanguage}`] === baseName  ? true : false,
       });
     });
   }
-}
+})
 
 //output as CSV
 csvWriter.writeRecords(result).then(() => {
